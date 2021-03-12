f7a80c6cb302f24d613f88bf7b92fa65d3b0a1d6
Samuel Leonard <34142670+Samueleonard@users.noreply.github.com>
2021-03-12 13:42:41 +0000

    menu improvements

--- Assets/Scripts/UnlockManager.cs
+++ Assets/Scripts/UnlockManager.cs
@@ -5,13 +5,22 @@ using UnityEngine.UI;
 
 public class UnlockManager : MonoBehaviour
 {
-    public Button level01Button, level02Button, level03Button, level04Button, level05Button;
-    public int levelPassed;
+    public Button level01Button,level02Button,level03Button,level04Button,level05Button;
+    int levelPassed;
 
     // Start is called before the first frame update
     void Start()
     {
         levelPassed = PlayerPrefs.GetInt("LevelPassed");
+       /*
+       for i less than max levels
+          gameobject.get(level + levelpassed).getcomponent<button>().interactable = false
+        TODO: fix if time, below works 
+        for(int i = 1; i <= levelPassed; i++){
+            GameObject current = GameObject.Find("LevelSelect (" + i + ")");
+            Debug.Log(current.name);
+        }
+        */
         level02Button.interactable = false;
         level03Button.interactable = false;
         level04Button.interactable = false;
